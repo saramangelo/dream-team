add296b86f01341299a277f0f6b4c90b3ea15e1e
saramangelo <saramangelo@gmail.com>
2022-12-21 19:59:35 -0500

    updated index.js array of questions, made index.html boilerplate

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 const inquirer = require("inquirer");
+
+// array of questions to prompt
+const questions = [
+  
+        {
+          type: "input",
+          message: "",
+          name: "",
+        },
+
+        {
+          type: "list",
+          message: "",
+          choices: [""],
+          name: "",
+        },
+
+]
+
+.prompt(questions)
