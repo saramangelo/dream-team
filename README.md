a56fa255ecb90c97c2ac96d2968decd355c12394
saramangelo <saramangelo@gmail.com>
2022-12-21 20:50:33 -0500

    updated index.js with functions to render questions

--- README.md
+++ README.md
@@ -16,10 +16,12 @@ THEN that GitHub profile opens in a new tab
 WHEN I start the application
 THEN I am prompted to enter the team manager’s name, employee ID, email address, and office number
 WHEN I enter the team manager’s name, employee ID, email address, and office number
-THEN I am presented with a menu with the option to add an engineer or an intern or to finish building my team
+THEN I am presented with a menu (LIST) with the option to add an engineer or an intern or to finish building my team
 WHEN I select the engineer option
 THEN I am prompted to enter the engineer’s name, ID, email, and GitHub username, and I am taken back to the menu
 WHEN I select the intern option
 THEN I am prompted to enter the intern’s name, ID, email, and school, and I am taken back to the menu
 WHEN I decide to finish building my team
 THEN I exit the application, and the HTML is generated
+
+// make classes, collect info from prompt. after ask question, go to prompt promise, instantiate the class (create an instance of the class), the parameters of the class will be the user input
--- index.js
+++ index.js
@@ -1,21 +1,136 @@
 const inquirer = require("inquirer");
+// require classes (files)
+const Intern = require("file that has Intern class")
 
+// every time I make a class, push data into this array
+const employees = [];
 // array of questions to prompt
-const questions = [
-  
-        {
-          type: "input",
-          message: "",
-          name: "",
-        },
+const mainQuestions = [
+
 
         {
           type: "list",
-          message: "",
-          choices: [""],
-          name: "",
+          message: "Which kind of employee would you like to add",
+          choices: ["Intern", "Engineer", "Manager", "Exit"],
+          name: "employeeType",
         },
 
 ]
+function generateHtml(data){
+const html = `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    ${data}
+</body>
+</html>`
+}
 
-.prompt(questions)
+// function to write README file
+function writeToFile(fileName, data) {
+    let htmlMarkup = "";
+    for (let i = 0; i < employees.length; i++){
+        // getRole is a method I'll create in classes that will return the type of employee
+        if(employees[i].getRole()==="Intern"){
+            htmlMarkup += `<h2>Intern</h2>`;
+        }
+        if(employees[i].getRole()==="Engineer"){
+            htmlMarkup += `<h2>Engineer</h2>`;
+        }
+        if(employees[i].getRole()==="Manager"){
+            htmlMarkup += `<h2>Manager</h2>`;
+        }
+    }
+  
+        fs.writeFile(fileName, generateHtml(htmlMarkup), (err) =>
+          err ? console.error(err) : console.log("index.html successfully created!")
+        );
+   
+  }
+
+  function askPromptQuestions(){
+    inquirer
+    .prompt(mainQuestions)
+
+    // data = giant object (answers from input)
+    .then((answers) => {
+      // console.log(answers);
+      if(answers.employeeType==="Intern"){
+        promptInternQuestions();
+      }
+      if(answers.employeeType==="Engineer"){
+        promptEngineerQuestions();
+      }
+      if(answers.employeeType==="Manager"){
+        promptManagerQuestions();
+      }
+    });
+  }
+
+  function promptInternQuestions(){
+   
+    const internQuestions = [
+
+
+     
+
+        // 4 questions: name, ID, email, and school
+]
+inquirer
+.prompt(internQuestions)
+.then((answers) => {
+    const intern = new Intern(answers.firstName, answers.lastName, answers.school);
+    employees.push(intern);
+    askPromptQuestions();
+  }
+)};
+
+
+  function promptEngineerQuestions(){
+   
+    const engineerQuestions = [
+
+
+
+        // name, ID, email, and github username
+]
+inquirer
+.prompt(engineerQuestions)
+.then((answers) => {
+    const engineer = new Engineer(answers.firstName, answers.lastName, answers.github);
+    employees.push(engineer);
+    askPromptQuestions();
+  }
+)};
+
+  function promptManagerQuestions(){
+   
+    const managerQuestions = [
+
+
+        // name, ID, email, and office number
+]
+inquirer
+.prompt(managerQuestions)
+.then((answers) => {
+    const manager = new Manager(answers.firstName, answers.lastName, answers.office);
+    employees.push(manager);
+    askPromptQuestions();
+  }
+)};
+  // referring to question with name: firstName, lastName, etc
+  // push this variable into an empty array
+  // separate files for intern class, engineer class, manager class, employee class
+  // employee class is parent, all others extend from it
+
+function exit(){
+    writeToFile("index.html", employees);
+}
+
+
+  askPromptQuestions();
